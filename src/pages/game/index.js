27d72f2ec7001aefc630993a413e1fa716489735
Muhammed Basaran <mbasaran0293@gmail.com>
2023-03-15 17:00:32 +0100

    create gaming page and add some styling

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,3 +1,44 @@
+import Link from "next/link";
+import Header from "../../../components/Header";
+import styled from "styled-components";
+
 export default function Game() {
-  return <h1>hello world</h1>;
+  return (
+    <StyledMain>
+      <Header />
+      <Link href="/">
+        <StyledExitButton> X </StyledExitButton>
+        <StyledScore>Score: </StyledScore>
+      </Link>
+    </StyledMain>
+  );
 }
+
+const StyledMain = styled.main`
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  color: white;
+  font-family: Public Pixel;
+  text-align: center;
+`;
+
+const StyledExitButton = styled.button`
+  position: absolute;
+  left: 40px;
+  margin-top: -5px;
+  padding: 5px;
+  font-size: 1.5rem;
+  background-color: black;
+  color: white;
+  font-family: Public Pixel;
+`;
+
+const StyledScore = styled.p`
+  margin-top: 50px;
+  font-size: 1.5rem;
+  background-color: black;
+  color: white;
+  font-family: Public Pixel;
+`;
